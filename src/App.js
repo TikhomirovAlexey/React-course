0dcf9a655b3796236fb31234c1c4a320fee0072c
Alexey <tihomir.94@mail.ru>
2024-06-30 22:01:54 +0300

    create new branch lesson-6 and added task to complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import TemperatureConverter from './components/TemperatureConverter';
-import TodoList from './components/TodoList ';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TemperatureConverter />
-        <TodoList />
-      </header>
+    <div>
     </div>
   );
 }
